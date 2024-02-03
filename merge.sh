a52cec222e4330ef03d5fb6b17b12028b48ea70c
Daniel Castro <danicc097@gmail.com>
2024-02-03 12:03:23 +0100

    update

--- merge.sh
+++ merge.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-yq eval-all '(
-        select(fi == 0).components.schemas
-        ) as $schemas
-        | (
-          select(fi == 1).components.schemas
-        ) as $new_schemas
-        | select(fi == 0)
-        | .components.schemas = $new_schemas *n $schemas
-        | .components.schemas += {
-              "Activity": {
-                "x-is-generated": true,
-                "$ref": "#/components/schemas/RestActivity"
-              }
-          }
-        ' file1.yaml generated.yaml >output.yaml
+# yq eval-all '(
+#         select(fi == 0).components.schemas
+#         ) as $schemas
+#         | (
+#           select(fi == 1).components.schemas
+#         ) as $new_schemas
+#         | select(fi == 0)
+#         | .components.schemas = $new_schemas *n $schemas
+#         | .components.schemas += {
+#               "Activity": {
+#                 "x-is-generated": true,
+#                 "$ref": "#/components/schemas/RestActivity"
+#               }
+#           }
+#         ' file1.yaml generated.yaml >output.yaml
 
-# yq e '.components.schemas.* |= . + {"x-is-generated": true}' file1.yaml >output.yaml
+yq e '.components.schemas.* |= . + {"x-is-generated": true}' file1.yaml >output.yaml
+yq e '(.components.schemas.* | key) line_comment="This is a generated schema"' -i output.yaml
